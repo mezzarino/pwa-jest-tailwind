b03889ea30c1027f67b4e08d578b652c5f7c99ef
James Merriman <mezzarino@outlook.com>
2023-07-13 14:48:55 +0100

    tweaks to manifest

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -15,7 +15,7 @@ export default function Home() {
       <header>
         <Header title="Travel guides" text="Our travel guides are jam packed with top tips and insider inspiration." />
       </header>
-      <section className="py-4 md:py-6 lg:py-8 px-4 lg:px-0 max-w-5xl mx-auto grid grid-cols-1 md:grid-cols-2 md:gap-4 lg:grid-cols-4 lg:gap-6">
+      <section className="py-4 md:py-6 lg:py-8 px-4 lg:px-0 max-w-5xl mx-auto grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-4 lg:gap-6">
 
       {cardData.map((card) => (
         <Card key={card.id} imageSrc={card.imageSrc} imageAlt={card.imageAlt} title={card.title} link={card.link} />
