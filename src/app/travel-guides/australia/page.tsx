4dcd6841b02cae44a50c042fcf8d97d822392098
James Merriman <mezzarino@outlook.com>
2023-07-13 15:56:02 +0100

    attempt to fix offline message

--- src/app/travel-guides/australia/page.tsx
+++ src/app/travel-guides/australia/page.tsx
@@ -1,29 +1,6 @@
-"use client"
-
-import { useState, useEffect } from "react";
 import Header from "../../components/header";
 
 export default function Guide() {
-  const [isOnline, setIsOnline] = useState(navigator.onLine);
-
-  useEffect(() => {
-    function onlineHandler() {
-      setIsOnline(true);
-    }
-
-    function offlineHandler() {
-      setIsOnline(false);
-    }
-
-    window.addEventListener("online", onlineHandler);
-    window.addEventListener("offline", offlineHandler);
-
-
-    return () => {
-      window.removeEventListener("online", onlineHandler);
-      window.removeEventListener("offline", offlineHandler);
-    };
-  }, []);
 
   return (
     <main className="flex flex-col justify-between">
@@ -32,9 +9,6 @@ export default function Guide() {
       </header>
       <section className="container px-4 mx-auto">
         <div className="max-w-5xl mx-auto text-left">
-          {!isOnline && (
-            <h2>You are offline and are viewing a saved version of this guide.</h2>
-          )}
           <p>Content</p>
         </div>
       </section>
