043b4f2445fa52fbb8997b074ed3df0f49992180
James Merriman <mezzarino@outlook.com>
2023-07-13 16:37:11 +0100

    Need to add the images as priority

--- src/app/components/card.tsx
+++ src/app/components/card.tsx
@@ -9,6 +9,7 @@ const Card = (props: { imageSrc: string; imageAlt: string; link: string; title: 
             <Link href={props.link}>
                 <div className="relative">
                     <Image
+                        priority
                         src={props.imageSrc}
                         alt={props.imageAlt}
                         width={400}
@@ -16,7 +17,7 @@ const Card = (props: { imageSrc: string; imageAlt: string; link: string; title: 
                     />
                 </div>
                 <div className="p-5">
-                    <h3 className="mb-2 text-xl font-bold tracking-tight text-white">{props.title}</h3>
+                    <h2 className="mb-2 text-xl font-bold tracking-tight text-white">{props.title}</h2>
 
                     <button className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-red-700 rounded-lg hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-800">
                         Read more <FontAwesomeIcon icon={faArrowRight} className="w-4 h-4 pl-1" /> 
